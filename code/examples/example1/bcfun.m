9a3f10dd1c57dfb85dcded71853e12db8abbca1b
Valentino Scalera <40667522+VaSca92@users.noreply.github.com>
2023-05-22 20:36:26 +0200

    Delete code/examples/example1 directory

--- code/examples/example1/bcfun.m
+++
@@ -1,8 +0,0 @@
-function [pl,ql,pr,qr] = bcfun(xl,ul,xr,ur,t)
-
-pl = [0;0]; 
-pr = [0;0]; 
-ql = [1;1];
-qr = [1;1]; 
-
-end
--- code/examples/example1/icfun.m
+++
@@ -1,3 +0,0 @@
-function u = icfun(x)
-   u =  [300 + 0*x ; 300 + 0*x];
-end
